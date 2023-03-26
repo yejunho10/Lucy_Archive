ec5921e0e7aa41b7bfe08b03b150d7760d1f4a90
yejunho10 <hyjcompany30@naver.com>
2023-03-26 12:11:35 +0900

    fix: Bugs

--- src/main/java/net/lucymc/archive/listener/EntityListener.java
+++ src/main/java/net/lucymc/archive/listener/EntityListener.java
@@ -1,18 +1,32 @@
 package net.lucymc.archive.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 
 public class EntityListener implements Listener {
     @EventHandler
-    public void onDamage(EntityDamageEvent event) {
+    public void onDamage(EntityDamageByEntityEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
+        if (!(event.getDamager() instanceof Player damager)) return;
+        if (damager.isOp()) return;
+
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onBlockDamage(EntityDamageByBlockEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+
         event.setCancelled(true);
     }
 
     @EventHandler
     public void onFoodLevelChange(FoodLevelChangeEvent event) {
+        if (!(event.getEntity() instanceof Player)) return;
         if (event.getFoodLevel() != 20) return;
 
         event.setCancelled(true);
